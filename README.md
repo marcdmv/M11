c9ee1860a2b0bfb2eec8376f2200f9aa6e48d845
Marc de Mas <marcdmv@gmail.com>
2020-11-23 11:08:04 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# M11
+
+### Spring Boot Initial Demo
+
+Aplicación inicial que de muestra:
+ - Flujo de inicialización
+ - Separación de capas
+ - Anotaciones Spring y carga de objetos
---
+++ src/main/java/springBootInitialDemo/controller/InitialController.java
@@ -0,0 +1,52 @@
+package springBootInitialDemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import springBootInitialDemo.dto.UserResponseDto;
+import springBootInitialDemo.service.IUserService;
+
+@RestController
+@RequestMapping("/v1")
+public class InitialController {
+
+    private final IUserService userService;
+
+    @Autowired
+    public InitialController(IUserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/test")
+    public String helloGradle() {
+        return "Hello Gradle!";
+    }
+
+    //@GetMapping("/")
+    //public String helloWorld() { return "HELLO WORLD"; }
+
+    @RequestMapping(path="/{NOM}")
+    public String helloWorld(@PathVariable("NOM") String name) {
+        String msg = String.format("HELLO %s", name);
+        return msg;
+    }
+
+    //@PutMapping(value ="", consumes = {"application/json"})
+    @GetMapping("/user/{uuid}")
+    public ResponseEntity<UserResponseDto> updatePrescription(
+            @PathVariable(name="uuid") String user) throws Exception {
+
+
+        UserResponseDto userResponseDto = userService.getUser("pp");
+
+        System.out.println(userResponseDto.getName());
+        System.out.println(userResponseDto.getSurname());
+        System.out.println(userResponseDto.getGender());
+
+        return new ResponseEntity<>(userResponseDto, HttpStatus.OK);
+
+    }
+
+
+}
